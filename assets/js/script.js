1bfe7c8e5d61f02f5da8903baead18bf20a152ad
maxh <maxhumpherys@gmail.com>
2021-10-16 01:41:35 -0600

    added 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,6 +93,70 @@ let searchCityBtnHandler = function () {
                     }
 
 
+                    // day 1 Forecast Date
+                    document.getElementById("day1ForecastDate").textContent = moment().add(1, 'days').format("M/DD/YYYY");
+                    // day 1 Forecast Image
+                    var weeklyForecastImage1 = document.getElementById("day1ForecastImage")
+                    weeklyForecastImage1.src = "http://openweathermap.org/img/w/" + data.daily[1].weather[0].icon + ".png";
+                    weeklyForecastImage1.classList.add("weeklyForecastImages");
+                    // day 1 Forecast weather info
+                    document.getElementById("day1ForecastTemp").textContent = "Temp: " + data.daily[1].temp.day + "\u00B0" + "F";
+                    document.getElementById("day1ForecastWind").textContent = "Wind: " + data.daily[1].wind_speed + " MPH";
+                    document.getElementById("day1ForecastHumidity").textContent = "Humidity: " + data.daily[1].humidity + " %";
+
+
+
+                    // day 2 Forecast Date
+                    document.getElementById("day2ForecastDate").textContent = moment().add(2, 'days').format("M/DD/YYYY");
+                    // day 2 Forecast Image
+                    var weeklyForecastImage2 = document.getElementById("day2ForecastImage")
+                    weeklyForecastImage2.src = "http://openweathermap.org/img/w/" + data.daily[2].weather[0].icon + ".png";
+                    weeklyForecastImage2.classList.add("weeklyForecastImages");
+                    // day 2 Forecast weather info
+                    document.getElementById("day2ForecastTemp").textContent = "Temp: " + data.daily[2].temp.day + "\u00B0" + "F";
+                    document.getElementById("day2ForecastWind").textContent = "Wind: " + data.daily[2].wind_speed + " MPH";
+                    document.getElementById("day2ForecastHumidity").textContent = "Humidity: " + data.daily[2].humidity + " %";
+
+
+
+                    // day 3 Forecast Date
+                    document.getElementById("day3ForecastDate").textContent = moment().add(3, 'days').format("M/DD/YYYY");
+                    // day 3 Forecast Image
+                    var weeklyForecastImage3 = document.getElementById("day3ForecastImage")
+                    weeklyForecastImage3.src = "http://openweathermap.org/img/w/" + data.daily[3].weather[0].icon + ".png";
+                    weeklyForecastImage3.classList.add("weeklyForecastImages");
+                    // day 3 Forecast weather info
+                    document.getElementById("day3ForecastTemp").textContent = "Temp: " + data.daily[3].temp.day + "\u00B0" + "F";
+                    document.getElementById("day3ForecastWind").textContent = "Wind: " + data.daily[3].wind_speed + " MPH";
+                    document.getElementById("day3ForecastHumidity").textContent = "Humidity: " + data.daily[3].humidity + " %";
+
+
+
+                    // day 4 Forecast Date
+                    document.getElementById("day4ForecastDate").textContent = moment().add(4, 'days').format("M/DD/YYYY");
+                    // day 4 Forecast Image
+                    var weeklyForecastImage4 = document.getElementById("day4ForecastImage")
+                    weeklyForecastImage4.src = "http://openweathermap.org/img/w/" + data.daily[4].weather[0].icon + ".png";
+                    weeklyForecastImage4.classList.add("weeklyForecastImages");
+                    // day 4 Forecast weather info
+                    document.getElementById("day4ForecastTemp").textContent = "Temp: " + data.daily[4].temp.day + "\u00B0" + "F";
+                    document.getElementById("day4ForecastWind").textContent = "Wind: " + data.daily[4].wind_speed + " MPH";
+                    document.getElementById("day4ForecastHumidity").textContent = "Humidity: " + data.daily[4].humidity + " %";
+
+
+
+                    // day 5 Forecast Date
+                    document.getElementById("day5ForecastDate").textContent = moment().add(5, 'days').format("M/DD/YYYY");
+                    // day 5 Forecast Image
+                    var weeklyForecastImage5 = document.getElementById("day5ForecastImage")
+                    weeklyForecastImage5.src = "http://openweathermap.org/img/w/" + data.daily[5].weather[0].icon + ".png";
+                    weeklyForecastImage5.classList.add("weeklyForecastImages");
+                    // day 5 Forecast weather info
+                    document.getElementById("day5ForecastTemp").textContent = "Temp: " + data.daily[5].temp.day + "\u00B0" + "F";
+                    document.getElementById("day5ForecastWind").textContent = "Wind: " + data.daily[5].wind_speed + " MPH";
+                    document.getElementById("day5ForecastHumidity").textContent = "Humidity: " + data.daily[5].humidity + " %";
+
+
                 })
         })
 
